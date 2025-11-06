4bd587b7039c58cc44afe884cebd8fd2b6766479
donzark <abdulrazakadekunle2@gmail.com>
2025-11-06 18:21:22 +0800

    fix langchain.chain

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_cohere import CohereEmbeddings
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_stuff_documents_chain
 
 
 load_dotenv()
